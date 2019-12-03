5b21846b6ab2e287daabbc2bc168cd336c4263b5
Naoumi1214 <wada121400@gmail.com>
2019-12-03 16:14:22 +0900

    ドラムロール再生開始後5.5秒後に乱数が表示されるようにした

--- js/random.js
+++ js/random.js
@@ -1,13 +1,25 @@
 $(function () {
+	//乱数を表示
+	const displayText = (randmath) => {
+		let target = Math.ceil(Math.random() * 100);
+		randmath.text(target);
+	}
+	
+	//数字を表示するh1
 	const randmath = $('#randmath');
+	//イベント開始ボタン
 	const btn = $('#btn');
-	const fadeinTime = 1000;
+	//フェード時間
+	const fadeinTime = 5500;
+	//オーディオオブジェクト
+	const audioElem = new Audio();
+	audioElem.src = 'audio/se_maoudamashii_instruments_drumroll.mp3';
 
 	btn.click(function () {
-		let target = Math.ceil(Math.random() * 100);
-
-		randmath.text(target);
-		randmath.hide().fadeIn(fadeinTime);
+		audioElem.play();
+		setTimeout(displayText, fadeinTime,randmath);
 	});
+
+	
 
 });
