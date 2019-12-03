9a0792cf1041f4cd7b97c7f45da556b264ea0a89
Naoumi1214 <wada121400@gmail.com>
2019-12-03 11:12:55 +0900

    ボタンを押したら、乱数が1秒分速度でフェードインをすることを実装した

--- js/random.js
+++ js/random.js
@@ -1,3 +1,13 @@
 $(function () {
-    const randmath = $('#randmath').text();
+	const randmath = $('#randmath');
+	const btn = $('#btn');
+	const fadeinTime = 1000;
+
+	btn.click(function () {
+		let target = Math.ceil(Math.random() * 100);
+
+		randmath.text(target);
+		randmath.hide().fadeIn(fadeinTime);
+	});
+
 });
